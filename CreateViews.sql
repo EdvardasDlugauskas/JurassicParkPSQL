bfbb688035c6bbcf3bf2a23c655330a062d53359
Edvardas Dlugauskas <edvardas.dlugauskas@gmail.com>
2018-11-28 14:58:42 +0200

    Fix Visit view (money sums)

--- CreateViews.sql
+++ CreateViews.sql
@@ -2,7 +2,12 @@ CREATE VIEW RegisteredVisitor
   AS SELECT *, AGE(Birthday) AS Age
   FROM _RegisteredVisitor;
 
+
+-- TODO check if this view works correctly
 CREATE VIEW Visit
-  AS SELECT *, SUM(VF.MoneySpent) + SUM(VE.TicketCost)
-  FROM _Visit, VisitUsesFacility AS VF, VisitBuysTicketEnclosure AS VE
-  WHERE VF.VisitId = _Visit.id AND VE.VisitId = _Visit.id;
+  AS SELECT _Visit.*, visitFacility.spent +  visitEnclosure.spent as MoneySpent
+  FROM _Visit
+       INNER JOIN (SELECT VisitId, SUM(MoneySpent) as spent FROM VisitUsesFacility GROUP BY VisitId)
+          visitFacility on _Visit.id = visitFacility.VisitId
+       INNER JOIN (SELECT VisitId, SUM(TicketCost) as spent FROM VisitBuysTicketEnclosure GROUP BY VisitId)
+          visitEnclosure on _Visit.id = visitEnclosure.VisitId;
