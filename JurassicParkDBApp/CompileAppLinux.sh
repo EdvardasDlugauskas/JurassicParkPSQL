ab0d50f62fcd74aec8c43a87f8a8f7c501e03f7e
MJay <marijusjasinskas65@gmail.com>
2018-12-08 12:39:33 +0200

    Removed old Db App project.

--- JurassicParkDBApp/CompileAppLinux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export CLASSPATH=$CLASSPATH:/usr/share/java/postgresql.jar
-javac -cp out -d out src/com/jp/Main.java src/com/jp/JpDbCommunicator.java src/com/jp/JpGui.java
-java -cp out com/jp/Main
