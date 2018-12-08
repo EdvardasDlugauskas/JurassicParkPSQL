d397c22aedfe63039f77aa3788cf659979a50dfd
MJay <marijusjasinskas65@gmail.com>
2018-12-08 14:13:21 +0200

    Updated compilation script

--- JpDbApp/RunApp.sh
+++ JpDbApp/RunApp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export CLASSPATH=$CLASSPATH:/usr/share/java/postgresql.jar
-javac -cp out -d out src/com/jp/Main.java src/com/jp/JpDbCommunicator.java src/com/jp/JpGui.java
+javac -cp out -d out src/com/jp/Main.java src/com/jp/JpDbCommunicator.java src/com/jp/JpGui.java src/com/jp/QueryExecutionResult.java
 java -cp out production/com/jp/Main
